dc1a8944577f3a20278e165fe16a2c355645d058
Kat Yevsukov <Katherine.Yevsukov@carvana.com>
2023-10-23 13:44:20 -0700

    wrote producer and consumer

--- KafkaProject/KafkaProducer/Program.cs
+++ KafkaProject/KafkaProducer/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Confluent.Kafka;
 
-app.MapGet("/", () => "Hello World!");
+var config = new ProducerConfig {
+    BootstrapServers = "localhost:9092",
+    SecurityProtocol = SecurityProtocol.Plaintext,
+};
 
-app.Run();
+using (var p = new ProducerBuilder<Null, string>(config).Build())
+{
+    while (true)
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            try
+            {
+                var toSend = $"This is message {i}";
+                var dr = await p.ProduceAsync("topic1", new Message<Null, string> { Value=toSend });
+                Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+
+        p.Flush(TimeSpan.FromSeconds(10));
+    }
+    
+}
